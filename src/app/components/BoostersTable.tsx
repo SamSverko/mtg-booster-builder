0e34378d71aab8a2c7bdc712368938709ad84f8c
Sam Sverko <sam.sverko@gmail.com>
2024-12-24 08:49:56 -0500

    feat(BoostersTable): add sorting

--- src/app/components/BoostersTable.tsx
+++ src/app/components/BoostersTable.tsx
@@ -1,26 +1,67 @@
 import {
     Box,
-    Chip,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableHead,
     TableRow,
+    TableSortLabel,
     Tooltip,
 } from "@mui/material";
+import { useState } from "react";
 import { PlayBoosterSerialized } from "@/app/types";
 import { FoilChip } from "@/app/components";
-import { PLAY_BOOSTER } from "@/app/constants";
 
 type BoostersTableProps = {
     boosters: PlayBoosterSerialized[];
 };
 
-// TODO - Allow "combine" by same setCode & number
-// TODO - Allow sort by columns
 // TODO - Allow hide name and rarity
 export default function BoostersTable({ boosters }: BoostersTableProps) {
+    const [order, setOrder] = useState<"asc" | "desc">("asc");
+    const [orderBy, setOrderBy] = useState<
+        "boosterIndex" | "collectorNumber" | "name" | "rarity"
+    >("boosterIndex");
+
+    const handleSort = (
+        property: "boosterIndex" | "collectorNumber" | "name" | "rarity"
+    ) => {
+        const isAsc = orderBy === property && order === "asc";
+        setOrder(isAsc ? "desc" : "asc");
+        setOrderBy(property);
+    };
+
+    // Flatten all cards with their booster index
+    const flattenedCards = boosters.flatMap((booster, boosterIndex) =>
+        booster.c.map((card) => ({
+            boosterIndex,
+            collectorNumber: card.c,
+            name: card.n,
+            foil: card.f,
+            rarity: card.r,
+        }))
+    );
+
+    // Sort the flattened list dynamically by the selected column
+    const sortedCards = [...flattenedCards].sort((a, b) => {
+        const aValue = a[orderBy];
+        const bValue = b[orderBy];
+
+        if (aValue === bValue && orderBy !== "collectorNumber") {
+            // Secondary sort by collector number for ties
+            return order === "asc"
+                ? a.collectorNumber - b.collectorNumber
+                : b.collectorNumber - a.collectorNumber;
+        }
+
+        if (order === "asc") {
+            return aValue < bValue ? -1 : 1;
+        } else {
+            return aValue > bValue ? -1 : 1;
+        }
+    });
+
     return (
         <Box
             flexGrow={1}
@@ -38,69 +79,79 @@ export default function BoostersTable({ boosters }: BoostersTableProps) {
                 <Table size="small" stickyHeader>
                     <TableHead>
                         <TableRow>
-                            <TableCell />
-                            <TableCell align="right">
-                                <Tooltip
-                                    placement="top"
-                                    title="Collector Number"
+                            <TableCell>
+                                <TableSortLabel
+                                    active={orderBy === "boosterIndex"}
+                                    direction={order}
+                                    onClick={() => handleSort("boosterIndex")}
                                 >
-                                    <Box component="span">#</Box>
-                                </Tooltip>
+                                    <Tooltip placement="top" title="Booster">
+                                        <Box component="span">B</Box>
+                                    </Tooltip>
+                                </TableSortLabel>
                             </TableCell>
-                            <TableCell>Name</TableCell>
+                            <TableCell align="right">
+                                <TableSortLabel
+                                    active={orderBy === "collectorNumber"}
+                                    direction={order}
+                                    onClick={() =>
+                                        handleSort("collectorNumber")
+                                    }
+                                >
+                                    <Tooltip
+                                        placement="top"
+                                        title="Collector Number"
+                                    >
+                                        <Box component="span">#</Box>
+                                    </Tooltip>
+                                </TableSortLabel>
+                            </TableCell>
+                            <TableCell>
+                                <TableSortLabel
+                                    active={orderBy === "name"}
+                                    direction={order}
+                                    onClick={() => handleSort("name")}
+                                >
+                                    <Box component="span">Name</Box>
+                                </TableSortLabel>
+                            </TableCell>
                             <TableCell align="center">
-                                <Tooltip placement="top" title="Rarity">
-                                    <Box component="span">R</Box>
-                                </Tooltip>
+                                <TableSortLabel
+                                    active={orderBy === "rarity"}
+                                    direction={order}
+                                    onClick={() => handleSort("rarity")}
+                                >
+                                    <Tooltip placement="top" title="Rarity">
+                                        <Box component="span">R</Box>
+                                    </Tooltip>
+                                </TableSortLabel>
                             </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {boosters.map((booster, boosterIndex) =>
-                            booster.c.map((card, cardIndex) => {
-                                const rarityLetter = card.r
-                                    .charAt(0)
-                                    .toUpperCase();
-
-                                return (
-                                    <TableRow
-                                        key={`${boosterIndex}-${cardIndex}`}
+                        {sortedCards.map((card, index) => (
+                            <TableRow key={index}>
+                                <TableCell align="center">
+                                    {card.boosterIndex + 1}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {card.collectorNumber}
+                                </TableCell>
+                                <TableCell>
+                                    <Box
+                                        alignItems="center"
+                                        display="flex"
+                                        gap={1}
                                     >
-                                        {cardIndex === 0 && (
-                                            <TableCell
-                                                align="center"
-                                                rowSpan={
-                                                    PLAY_BOOSTER.slots.length
-                                                }
-                                                sx={{
-                                                    verticalAlign: "top",
-                                                }}
-                                            >
-                                                {boosterIndex + 1}
-                                            </TableCell>
-                                        )}
-                                        <TableCell align="right">
-                                            {card.c}
-                                        </TableCell>
-                                        <TableCell>
-                                            <Box
-                                                alignItems="center"
-                                                display="flex"
-                                                gap={1}
-                                            >
-                                                {card.n}
-                                                {card.f !== "normal" && (
-                                                    <FoilChip />
-                                                )}
-                                            </Box>
-                                        </TableCell>
-                                        <TableCell align="center">
-                                            {rarityLetter}
-                                        </TableCell>
-                                    </TableRow>
-                                );
-                            })
-                        )}
+                                        {card.name}
+                                        {card.foil !== "normal" && <FoilChip />}
+                                    </Box>
+                                </TableCell>
+                                <TableCell align="center">
+                                    {card.rarity.charAt(0).toUpperCase()}
+                                </TableCell>
+                            </TableRow>
+                        ))}
                     </TableBody>
                 </Table>
             </TableContainer>

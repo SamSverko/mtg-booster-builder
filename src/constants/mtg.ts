954e745c53b58a694e6963848f4d7f8f170e3bf6
Sam Sverko <sam.sverko@gmail.com>
2024-12-26 22:04:40 -0500

    feat(constants/mtg): add BLB Play Booster

--- src/constants/mtg.ts
+++ src/constants/mtg.ts
@@ -108,7 +108,7 @@ export const PLAY_BOOSTER: App.PlayBooster = {
             { foil: "normal", percentage: 14.29, rarity: "mythic" },
         ],
         // 12 - Basic Land
-        // TODO - I think nonbasic lands can also appear here?
+        // TODO - How to handle nonbasic lands when I don't have access to the type?
         [
             {
                 foil: "normal",
@@ -138,6 +138,343 @@ export const PLAY_BOOSTER: App.PlayBooster = {
             { foil: "foil", percentage: 35.29, rarity: "uncommon" },
             { foil: "foil", percentage: 5.15, rarity: "rare" },
             { foil: "foil", percentage: 0.74, rarity: "mythic" },
+        ],
+    ],
+};
+
+/**
+ * Special Guests (SPG) set.
+ *
+ * Sourced from [Scryfall](https://scryfall.com/sets/spg).
+ */
+export const THE_LIST = {
+    blb: [54, 55, 56, 57, 58, 59, 60, 61, 62, 63].map((collectorNumber) => ({
+        collectorNumber,
+        setCode: "spg",
+    })),
+};
+
+export const FULL_ART_BASIC_LANDS_BLB = {
+    spring: [262, 266, 270, 274, 278].map((collectorNumber) => ({
+        collectorNumber,
+        setCode: "blb",
+    })),
+    summer: [263, 267, 271, 275, 279].map((collectorNumber) => ({
+        collectorNumber,
+        setCode: "blb",
+    })),
+    autumn: [264, 268, 272, 276, 280].map((collectorNumber) => ({
+        collectorNumber,
+        setCode: "blb",
+    })),
+    winter: [265, 269, 273, 277, 281].map((collectorNumber) => ({
+        collectorNumber,
+        setCode: "blb",
+    })),
+};
+
+export const NONBASIC_LANDS_BLB = {
+    common: [254, 261, 396, 397].map((collectorNumber) => ({
+        collectorNumber,
+        setCode: "blb",
+    })),
+    uncommon: [255, 256, 257, 258, 259].map((collectorNumber) => ({
+        collectorNumber,
+        setCode: "blb",
+    })),
+    rare: [252, 253, 260, 337, 338, 339, 340, 367, 368].map(
+        (collectorNumber) => ({
+            collectorNumber,
+            setCode: "blb",
+        })
+    ),
+};
+
+/**
+ * Bloomburrow Play Booster, excluding non-playable card slot.
+ *
+ * Slot info sourced from [Collecting Bloomburrow](https://magic.wizards.com/en/news/feature/collecting-bloomburrow).
+ */
+export const PLAY_BOOSTER_BLB: App.PlayBooster = {
+    slots: [
+        // 1 - Common
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "common",
+            },
+        ],
+        // 2 - Common
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "common",
+            },
+        ],
+        // 3 - Common
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "common",
+            },
+        ],
+        // 4 - Common
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "common",
+            },
+        ],
+        // 5 - Common
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "common",
+            },
+        ],
+        // 6 - Common
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "common",
+            },
+        ],
+        // 7 - Common or The List
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 98.5,
+                rarity: "common",
+            },
+            {
+                allowList: THE_LIST.blb,
+                foil: "normal",
+                percentage: 1.5,
+                rarity: "mythic",
+            },
+        ],
+        // 8 - Uncommon
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "uncommon",
+            },
+        ],
+        // 9 - Uncommon
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "uncommon",
+            },
+        ],
+        // 10 - Uncommon
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 100,
+                rarity: "uncommon",
+            },
+        ],
+        // 11 - Wildcard of any rarity
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 58.82,
+                rarity: "common",
+            },
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 35.29,
+                rarity: "uncommon",
+            },
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                ],
+                foil: "normal",
+                percentage: 5.15,
+                rarity: "rare",
+            },
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 0.74,
+                rarity: "mythic",
+            },
+        ],
+        // 12 - Rare or Mythic Rare
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                ],
+                foil: "normal",
+                percentage: 85.71,
+                rarity: "rare",
+            },
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "normal",
+                percentage: 14.29,
+                rarity: "mythic",
+            },
+        ],
+        // 13 - Traditional foil of any rarity
+        [
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "foil",
+                percentage: 58.82,
+                rarity: "common",
+            },
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "foil",
+                percentage: 35.29,
+                rarity: "uncommon",
+            },
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                ],
+                foil: "foil",
+                percentage: 5.15,
+                rarity: "rare",
+            },
+            {
+                denyList: [
+                    ...NONBASIC_LANDS_BLB.common,
+                    ...NONBASIC_LANDS_BLB.uncommon,
+                    ...NONBASIC_LANDS_BLB.rare,
+                ],
+                foil: "foil",
+                percentage: 0.74,
+                rarity: "mythic",
+            },
+        ],
+        // 14 - Seasonal full-art basic land
+        [
+            {
+                allowList: FULL_ART_BASIC_LANDS_BLB.spring,
+                foil: "normal",
+                percentage: 32,
+                rarity: "common",
+            },
+            {
+                allowList: FULL_ART_BASIC_LANDS_BLB.spring,
+                foil: "foil",
+                percentage: 8,
+                rarity: "common",
+            },
+            {
+                allowList: FULL_ART_BASIC_LANDS_BLB.summer,
+                foil: "normal",
+                percentage: 24,
+                rarity: "common",
+            },
+            {
+                allowList: FULL_ART_BASIC_LANDS_BLB.summer,
+                foil: "foil",
+                percentage: 6,
+                rarity: "common",
+            },
+            {
+                allowList: FULL_ART_BASIC_LANDS_BLB.autumn,
+                foil: "normal",
+                percentage: 16,
+                rarity: "common",
+            },
+            {
+                allowList: FULL_ART_BASIC_LANDS_BLB.autumn,
+                foil: "foil",
+                percentage: 4,
+                rarity: "common",
+            },
+            {
+                allowList: FULL_ART_BASIC_LANDS_BLB.winter,
+                foil: "normal",
+                percentage: 8,
+                rarity: "common",
+            },
+            {
+                allowList: FULL_ART_BASIC_LANDS_BLB.winter,
+                foil: "foil",
+                percentage: 2,
+                rarity: "common",
+            },
         ],
     ],
 };
--- src/types/app.ts
+++ src/types/app.ts
@@ -22,16 +22,53 @@ export type AllocatedBoosterCountBySetCode = CardCountBySetCode;
 /**
  * Play Boosters
  */
+
+/**
+ * A single card option for inclusion or exclusion in a Play Booster slot.
+ */
+export type PlayBoosterSlotItemOption = {
+    collectorNumber: ManaBox.Card["collectorNumber"];
+    setCode: ManaBox.Card["setCode"];
+};
+
+/**
+ * Represents a single slot in a Play Booster with rules for allowed or denied card options.
+ */
 export type PlayBoosterSlotItem = {
     foil: ManaBox.CardFoil;
     percentage: number;
     rarity: ManaBox.CardRarity;
+    /**
+     * A list of specific cards that are explicitly allowed in this slot.
+     * If provided, the selected card must match one of these options.
+     */
+    allowList?: PlayBoosterSlotItemOption[];
+    /**
+     * A list of specific cards that are explicitly denied for this slot.
+     * If provided, the selected card must not match any of these options.
+     */
+    denyList?: PlayBoosterSlotItemOption[];
+    /**
+     * @deprecated - Kinda? It's needed for the generic Play Booster, but use allowList and denyList instead for set-specific Play Boosters.
+     */
     superType?: MTG.CardSupertype;
+    /**
+     * @deprecated - Kinda? It's needed for the generic Play Booster, but use allowList and denyList instead for set-specific Play Boosters.
+     */
     type?: MTG.CardType;
 }[];
 
+/**
+ * Represents a Play Booster, containing a set code and a list of slot configurations.
+ */
 export type PlayBooster = {
+    /**
+     * The set code for the booster, if applicable.
+     */
     setCode?: string;
+    /**
+     * The list of slots in this booster, defining rarity, types, and constraints.
+     */
     slots: PlayBoosterSlotItem[];
 };
 

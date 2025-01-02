f15eefd1bb74368356b4621f7af66224fca260ce
Sam Sverko <sam.sverko@gmail.com>
2025-01-01 19:32:53 -0500

    test(utils): fix generateMockLibrary

--- src/utils/generateBoosters/generateBoosters.test.ts
+++ src/utils/generateBoosters/generateBoosters.test.ts
@@ -1,155 +1,43 @@
-import {
-    BASIC_LANDS_FULL_ART_BLB,
-    NONBASIC_LANDS_BLB,
-    PLAY_BOOSTER_RULE,
-    THE_LIST,
-} from "@/constants";
+import { PLAY_BOOSTER_RULE } from "@/constants";
 import { generateBoosters } from "@/utils";
-import { generateMockCards, generateMockLibrary } from "@/utils/test-utils";
+import { generateMockLibrary } from "@/utils/test-utils";
 
-const theListCards = generateMockCards({
-    cardProps: THE_LIST.BLB,
-});
+describe("generateBoosters", () => {
+    const setCode1 = "ABC";
+    const setCode2 = "DEF";
 
-const blbCards = [
-    ...generateMockLibrary({ sets: [{ setCode: "BLB" }] }),
-    ...generateMockCards({
-        cardProps: [
-            ...BASIC_LANDS_FULL_ART_BLB.autumn.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "normal" as const,
-                    name: `Mock Full Art Basic Land Card - Autumn ${index}`,
-                };
-            }),
-            ...BASIC_LANDS_FULL_ART_BLB.autumn.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "foil" as const,
-                    name: `Mock Full Art Basic Land Foil Card - Autumn ${index}`,
-                };
-            }),
-            ...BASIC_LANDS_FULL_ART_BLB.spring.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "normal" as const,
-                    name: `Mock Full Art Basic Land Card - Spring ${index}`,
-                };
-            }),
-            ...BASIC_LANDS_FULL_ART_BLB.spring.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "foil" as const,
-                    name: `Mock Full Art Basic Land Foil Card - Spring ${index}`,
-                };
-            }),
-            ...BASIC_LANDS_FULL_ART_BLB.summer.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "normal" as const,
-                    name: `Mock Full Art Basic Land Card - Summer ${index}`,
-                };
-            }),
-            ...BASIC_LANDS_FULL_ART_BLB.summer.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "foil" as const,
-                    name: `Mock Full Art Basic Land Foil Card - Summer ${index}`,
-                };
-            }),
-            ...BASIC_LANDS_FULL_ART_BLB.winter.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "normal" as const,
-                    name: `Mock Full Art Basic Land Card - Winter ${index}`,
-                };
-            }),
-            ...BASIC_LANDS_FULL_ART_BLB.winter.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "foil" as const,
-                    name: `Mock Full Art Basic Land Foil Card - Winter ${index}`,
-                };
-            }),
+    const singleSetLibrary = generateMockLibrary({
+        sets: [{ setCode: setCode1 }],
+    });
+
+    const multiSetLibrary = generateMockLibrary({
+        sets: [{ setCode: setCode1 }, { setCode: setCode2 }],
+    });
+
+    const insufficentCardCountLibrary = generateMockLibrary({
+        sets: [
+            {
+                setCode: setCode1,
+                commonCardCount: 1,
+                uncommonCardCount: 1,
+                rareCardCount: 1,
+                mythicCardCount: 1,
+                commonFoilCardCount: 1,
+                uncommonFoilCardCount: 1,
+                rareFoilCardCount: 1,
+                mythicFoilCardCount: 1,
+                basicLandCardCount: 1,
+                basicLandFoilCardCount: 1,
+            },
         ],
-    }),
-    ...generateMockCards({
-        cardProps: [
-            ...NONBASIC_LANDS_BLB.common.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "normal" as const,
-                    name: `Mock Common Nonbasic Land Card ${index}`,
-                };
-            }),
-            ...NONBASIC_LANDS_BLB.common.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "foil" as const,
-                    name: `Mock Common Nonbasic Land Foil Card ${index}`,
-                };
-            }),
-            ...NONBASIC_LANDS_BLB.uncommon.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "normal" as const,
-                    name: `Mock Uncommon Nonbasic Land Card ${index}`,
-                };
-            }),
-            ...NONBASIC_LANDS_BLB.uncommon.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "normal" as const,
-                    name: `Mock Uncommon Nonbasic Land Foil Card ${index}`,
-                };
-            }),
-            ...NONBASIC_LANDS_BLB.rare.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "normal" as const,
-                    name: `Mock Rare Nonbasic Land Card ${index}`,
-                };
-            }),
-            ...NONBASIC_LANDS_BLB.rare.map((card, index) => {
-                return {
-                    ...card,
-                    foil: "foil" as const,
-                    name: `Mock Rare Nonbasic Land Foil Card ${index}`,
-                };
-            }),
-        ],
-    }),
-];
+    });
 
-const insufficentCardCountLibrary = generateMockLibrary({
-    sets: [
-        {
-            setCode: "BLB",
-            commonCardCount: 1,
-            uncommonCardCount: 1,
-            rareCardCount: 1,
-            mythicCardCount: 1,
-            commonFoilCardCount: 1,
-            uncommonFoilCardCount: 1,
-            rareFoilCardCount: 1,
-            mythicFoilCardCount: 1,
-            basicLandCardCount: 1,
-            basicLandFoilCardCount: 1,
-        },
-    ],
-});
-
-const multiSetLibrary = generateMockLibrary({
-    sets: [{ setCode: "abc" }, { setCode: "def" }],
-});
-
-describe("getCardCount", () => {
     it("should generate boosters without errors", () => {
         const boosters = generateBoosters({
             allocatedBoosterCountBySetCode: {
-                BLB: 2,
+                [setCode1]: 2,
             },
-            cards: [...blbCards, ...theListCards],
+            cards: singleSetLibrary,
         });
 
         expect(boosters.errors).toEqual([]);
@@ -162,7 +50,7 @@ describe("getCardCount", () => {
 
     it("should return an error if no cards are provided", () => {
         const boosters = generateBoosters({
-            allocatedBoosterCountBySetCode: { BLB: 1 },
+            allocatedBoosterCountBySetCode: { [setCode1]: 1 },
             cards: [],
         });
 
@@ -172,8 +60,8 @@ describe("getCardCount", () => {
 
     it("should return an error if no cards match the set codes", () => {
         const boosters = generateBoosters({
-            allocatedBoosterCountBySetCode: { BLB: 1 },
-            cards: generateMockCards({ cardProps: [{ setCode: "xyz" }] }),
+            allocatedBoosterCountBySetCode: { [setCode2]: 1 },
+            cards: singleSetLibrary,
         });
 
         expect(boosters.errors).toContain(
@@ -184,19 +72,19 @@ describe("getCardCount", () => {
 
     it("should handle the case where no boosters are allocated", () => {
         const boosters = generateBoosters({
-            allocatedBoosterCountBySetCode: { BLB: 0 },
-            cards: blbCards,
+            allocatedBoosterCountBySetCode: { [setCode1]: 0 },
+            cards: singleSetLibrary,
         });
 
         expect(boosters.errors).toContain(
-            "Skipping BLB as allocated booster count is 0 or less."
+            "Skipping ABC as allocated booster count is 0 or less."
         );
         expect(boosters.boosters.length).toBe(0);
     });
 
     it("should return an error if there are not enough cards available for a booster slot", () => {
         const boosters = generateBoosters({
-            allocatedBoosterCountBySetCode: { BLB: 1 },
+            allocatedBoosterCountBySetCode: { [setCode1]: 1 },
             cards: insufficentCardCountLibrary,
         });
 
@@ -208,7 +96,7 @@ describe("getCardCount", () => {
 
     it("should generate boosters for multiple set codes", () => {
         const boosters = generateBoosters({
-            allocatedBoosterCountBySetCode: { abc: 2, def: 1 },
+            allocatedBoosterCountBySetCode: { [setCode1]: 2, [setCode2]: 1 },
             cards: multiSetLibrary,
         });
 
@@ -219,11 +107,11 @@ describe("getCardCount", () => {
     it("each booster should not contain duplicate cards", () => {
         const boosterCount = 2;
         const boosters = generateBoosters({
-            allocatedBoosterCountBySetCode: { abc: boosterCount },
+            allocatedBoosterCountBySetCode: { [setCode1]: boosterCount },
             cards: generateMockLibrary({
                 sets: [
                     {
-                        setCode: "abc",
+                        setCode: setCode1,
                         commonCardCount: 9 * boosterCount,
                         uncommonCardCount: 5 * boosterCount,
                         rareCardCount: 3 * boosterCount,
@@ -243,6 +131,46 @@ describe("getCardCount", () => {
             const cardNames = booster.cards.map((card) => card.name);
             const uniqeCardNames = new Set(cardNames);
             expect(uniqeCardNames.size).toBe(cardNames.length);
+        });
+    });
+
+    it("should ensure that all cards in a GENERIC booster match their respective rule slot", () => {
+        const boosterCount = 2;
+        const boosters = generateBoosters({
+            allocatedBoosterCountBySetCode: { [setCode1]: boosterCount },
+            cards: generateMockLibrary({
+                sets: [
+                    {
+                        setCode: setCode1,
+                        commonCardCount: 9 * boosterCount,
+                        uncommonCardCount: 5 * boosterCount,
+                        rareCardCount: 3 * boosterCount,
+                        mythicCardCount: 3 * boosterCount,
+                        commonFoilCardCount: 2 * boosterCount,
+                        uncommonFoilCardCount: 2 * boosterCount,
+                        rareFoilCardCount: 2 * boosterCount,
+                        mythicFoilCardCount: 2 * boosterCount,
+                        basicLandCardCount: 2 * boosterCount,
+                        basicLandFoilCardCount: 2 * boosterCount,
+                    },
+                ],
+            }),
+        });
+
+        boosters.boosters.forEach((booster) => {
+            booster.cards.forEach((card, index) => {
+                const selectedSlot = PLAY_BOOSTER_RULE.slots[index];
+
+                // Check if the card matches at least one configuration in the selected slot
+                const matchFound = selectedSlot.some(
+                    (slotConfig) =>
+                        slotConfig.rarity === card.rarity &&
+                        slotConfig.foil === card.foil
+                );
+
+                expect(matchFound).toBe(true);
+                expect(card.setCode).toBe(setCode1);
+            });
         });
     });
 });
